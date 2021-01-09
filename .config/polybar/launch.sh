44165dff03a3886ce3caa77dc7fd412c61812bd4
Utkarsh Gupta <utkarsh@debian.org>
2021-01-09 12:27:55 +0530

    Refresh .config/polybar/

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example >> /tmp/polybar_example.log 2>&1 &
 
-echo "Polybar launched..."
+echo "Bars launched..."
